26989a2051b5feec07e25eb658cba0920f99e1d0
Luke Miller <88352100+LA-Miller@users.noreply.github.com>
2025-11-08 14:28:43 -0500

    initial commit

---
+++ cocktail_cart.js
@@ -0,0 +1,29 @@
+const drinks = [
+    {
+        name: "Hot Toddy",
+        recipe: "Dissolve honey in boiling water, add liquor, lemon juice, cloves, and a cinnamon stick.",
+        calories: 100,
+        image: "images/hot_toddy.png"
+    },
+    {
+        name: "Hot Buttered Rum",
+        recipe: "Mix spiced butter, hot water (or apple cider), and rum.",
+        calories: 250,
+        image: "images/hot_buttered_rum.png"
+    },
+    {
+        name: "Peppermint White Russian",
+        recipe: "Pour vodka and Kahlua on ice, top with milk or cream and a hint of peppermint. Garnish with candy cane.",
+        calories: 170
+    },
+    {
+        name: "Christmas Old Fashioned",
+        recipe: "Mix bourbon, lemon juice, orange juice, bitters, and spiced syrup. Garnish with an orange slice.",
+        calories: 120
+    },
+    {
+        name: "Cranberry Mimosa",
+        recipe: "Mix cranberry juice and orange liquer, top with sparkling wine.",
+        calories: 150
+    }
+]
